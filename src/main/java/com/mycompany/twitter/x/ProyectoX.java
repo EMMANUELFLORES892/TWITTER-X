21af89211388acf3ed438513448ad57c0139af0c
DELL <DELL@148.210.239.110>
2024-02-19 18:58:28 -0700

    ultima actualizacion del repositorio 19-02-24

---
+++ src/main/java/com/mycompany/twitter/x/ProyectoX.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.twitter.x;
+
+/**
+ *
+ * @author user
+ */
+public class ProyectoX {
+
+    public static void main(String[] args) {
+        System.out.println("Prueba 5!");
+        //XD
+    }
+}
+//https://www.youtube.com/watch?v=qHQXUrrubcs -> Como hacer y subir cosas al repositotio
+// https://www.youtube.com/watch?v=LdDVi6_HdVs -> solucion de error que me salio, algo de las branches
+
