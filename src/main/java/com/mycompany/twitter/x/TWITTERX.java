bd04505a97e2254b1eeebd1b4514db805504a069
DELL <DELL@148.210.239.110>
2024-02-19 18:54:18 -0700

    modificacion de main

--- src/main/java/com/mycompany/twitter/x/TWITTERX.java
+++ src/main/java/com/mycompany/twitter/x/TWITTERX.java
@@ -11,6 +11,6 @@ package com.mycompany.twitter.x;
 public class TWITTERX {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("push");
     }
 }
